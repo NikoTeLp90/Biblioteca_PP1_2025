1e8c3478e5cc6a108420a56b965aab3981327b6d
Nacho <ignaciogimenez511@gmail.com>
2025-09-18 17:49:08 -0300

    lo hecho en clase(falta editar alumno)

--- php/agregar_alumno.php
+++ php/agregar_alumno.php
@@ -3,11 +3,14 @@
 include '../sql/db_functions.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
-    $nombre = trim($_POST["nombre"]);
-    $apellido = trim($_POST["apellido"]);
-    $dni = trim ($_POST["dni"]);
+    $nombre =trim($_POST["nombre"]);
+    $apellido =trim($_POST["apellido"]);
+    $email =trim($_POST["email"]);
+    $cargo =trim($_POST["cargo"]);
+    $contrasena =trim($_POST["contrasena"]);
 
-    crearAlumno($conexion, $nombre, $apellido, $dni);
+
+    crearUsuario($conexion, $nombre, $apellido, $email, $cargo, $contrasena);
 }
 
 
@@ -15,11 +18,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 
 <html>
 <head>
-    <title>Crear alumno</title>
+    <title>Crear Usuario</title>
 </head>
 
 <body>
-    <h1>Crear alumno</h1>
+    <h1>Crear Usuario</h1>
 
     <form action="../php/agregar_alumno.php" method="post">
 
@@ -31,8 +34,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
         <input type="text" name="apellido" placeholder="Ingrese un apellido" required>
         <br>
 
-        <label for="dni">DNI:</label>
-        <input type="text" name="dni" required><br>
+        <label for="email">Email:</label>
+        <input type="text" name="email" required><br>
+
+        <label for="cargo">Cargo:</label>
+        <select name="cargo">
+            <option value="bibliotecario"> Bibliotecario </option>
+            <option value="admin"> Administrador </option>
+            <option value="secretario"> Secretario </option>
+            </select>
+        <br>
+        
+        <label for="contrasena">Contrasena::</label>
+        <input type="password" name="contrasena" required><br>
 
         <input type="submit" value="Guardar">
         <button type="button" >Ir al Index</button>
--- sql/db_functions.php
+++ sql/db_functions.php
@@ -2,11 +2,11 @@
 include '../config/config.php';
 include '../config/connect.php';
 
-function crearAlumno($conexion, $nombre, $apellido, $dni){
-    $query = "INSERT INTO alumnos (nombre, apellido, dni) VALUES (?,?,?)";
+function crearUsuario($conexion, $nombre, $apellido, $email, $cargo, $contrasena){
+    $query = "INSERT INTO usuario (nombre, apellido, email, cargo, contrasena) VALUES (?,?,?,?,?)";
 
     if ($stmt = $conexion->prepare($query) ){
-        $stmt ->bind_param("sss", $nombre, $apellido, $dni);
+        $stmt ->bind_param("sssss", $nombre, $apellido, $email, $cargo, $contrasena);
 
         if($stmt->execute()){
             echo "Alumno creado correctamente";
@@ -20,3 +20,23 @@ function crearAlumno($conexion, $nombre, $apellido, $dni){
         echo "Error " . $query . "<br>" . $conexion ->error;
     }
 }
+
+function editarUsuarios($conexion, $id, $nombre, $apellido, $email, $cargo, $contrasena){
+    $query = "UPDATE usuario SET nombre = ?, apellido = ?, cargo = ?, contraseña = ? WHERE id = ?";
+    
+    if ($stmt = $conexion->prepare($query) ){
+        $stmt ->bind_param("sssssi", $nombre, $apellido, $email, $cargo, $contrasena);
+
+        if($stmt->execute()){
+            echo "Alumno creado correctamente";
+            
+            exit();
+        }else {
+            echo "Error al crear alumno: ". $stmt ->error;
+
+        }
+    }else {
+        echo "Error " . $query . "<br>" . $conexion ->error;
+    }
+}
+
