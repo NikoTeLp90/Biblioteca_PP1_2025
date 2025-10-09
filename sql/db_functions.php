a42ccfe879407311ddd3fe2779dea4ec2b52b98c
NikoTeLp90 <nikote.lp@gmail.com>
2025-10-09 16:34:38 -0300

    bd insumos

--- sql/db_functions.php
+++ sql/db_functions.php
@@ -106,5 +106,83 @@ if ($stmtFicha = $conexion->prepare($sqlFicha)) {
         echo "Error al preparar la consulta: " . $conexion->error;
     }
 }
+// INSUMOS ------------------------
+
+function agregarInsumo($conexion, $nombre, $categoria, $disponibilidad, $estado, $observaciones) {
+    $query = "INSERT INTO insumo (nombre, categoria, disponibilidad, estado, observaciones) VALUES (?,?,?,?,?)";
+
+    if ($stmt = $conexion->prepare($query)) {
+        $stmt->bind_param("sssss", $nombre, $categoria, $disponibilidad, $estado, $observaciones); // bindear (apunta) para asegurar que los elementos sean los correctos "sss" es string-string-string
+
+        if ($stmt->execute()) {
+            echo "Insumo cargado correctamente";
+            echo '<br>';
+            echo '<a href="../index.html">Ir al Index</button>';
+            echo '<br>';
+            echo '<a href="listar_insumos.php">Ver insumos</a>';
+            exit();
+        } else {
+            echo "Error al cargar insumo: ". $stmt->error;
+        }
+    } else {
+        echo "Error: " . $query . "<br>" . $conexion->error;
+    }
+}
+
+function editarInsumo($conexion, $id, $nombre, $categoria, $disponibilidad, $estado, $observaciones) {
+    $query = "UPDATE insumo SET nombre = ?, categoria = ?, disponibilidad = ?, estado = ?, observaciones = ? WHERE id = ?;";
+
+    if ($stmt = $conexion->prepare($query)) {
+        $stmt->bind_param("sssssi", $nombre, $categoria, $disponibilidad, $estado, $observaciones, $id);
+
+        if ($stmt->execute()) {
+            echo "Insumo actualizado correctamente";
+            // Opcional: redirigir a la lista de alumnos
+            header("Location: listar_insumos.php");
+            exit();
+        } else {
+            echo "Error al actualizar insumo: " . $stmt->error;
+        }
+
+        $stmt->close();
+    } else {
+        echo "Error al preparar la consulta: " . $conexion->error;
+    }
+}
+
+function obtenerInsumos($conexion): array {
+    $sql = "SELECT * FROM insumo;";
+    $result = $conexion ->query($sql);
+    $insumos = [];
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $insumos[] = $row;
+        }
+    }
+    return $insumos;
+}
+
+function eliminarInsumo($conexion, $id) {
+
+    $sql = "DELETE FROM insumo WHERE id = ?;";
+
+    // Preparar la sentencia
+    if ($stmt = $conexion->prepare($sql)) {
+        // Vincular el parámetro
+        $stmt->bind_param("i", $id);
+
+        // Ejecutar la consulta
+        if ($stmt->execute()) {
+            echo "Insumo eliminado correctamente";
+        } else {
+            echo "Error al eliminar insumo: " . $stmt->error;
+        }
+
+        // Cerrar la sentencia
+        $stmt->close();
+    } else {
+        echo "Error al preparar la consulta: " . $conexion->error;
+    }
+}
 
 ?>
